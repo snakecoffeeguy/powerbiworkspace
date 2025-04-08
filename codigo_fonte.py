31c3ef2296f3ae63338dc9b7258accad94a7f8dc
snakecoffeeguy <snakecoffeeguy@gmail.com>
2025-04-08 13:40:52 -0300

    Update codigo_fonte.py

--- codigo_fonte.py
+++ codigo_fonte.py
@@ -1,8 +1,7 @@
 import networkx as nx
 from pyvis.network import Network
-import os
 
-# Dados fictícios (poderiam vir de um CSV exportado do Power BI, por exemplo)
+# Dados atualizados
 tabelas = ['Clientes', 'Produtos', 'Vendas', 'Pagamentos']
 conexoes = [('Clientes', 'Vendas'), ('Produtos', 'Vendas'), ('Vendas', 'Pagamentos')]
 
@@ -13,9 +12,4 @@ G.add_edges_from(conexoes)
 
 net = Network(height='750px', width='100%', bgcolor='#222222', font_color='white')
 net.from_nx(G)
-net.show('index.html')
-
-# Git automation (opcional)
-os.system('git add index.html')
-os.system('git commit -m "Atualização automática do grafo"')
-os.system('git push')
+net.show('index.html')  # isso é essencial!
